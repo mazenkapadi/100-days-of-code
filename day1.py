60cfa2a7a74bf59bac775f3dfc6f53187bd6d016
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-05-22 18:17:27 -0400

    Completed Day 1

---
+++ day1.py
@@ -0,0 +1,5 @@
+#Day 1 band name generator
+print("Welcome to the Band Name Gnarator!")
+street = input("What city did you grow up in?")
+pet = input("What was your first pet?")
+print("Your band has to be called " + street + " " + pet +"s")
